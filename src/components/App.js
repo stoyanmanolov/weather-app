9356aa4671948f2327e23c84947ca99df53a17b1
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2019-07-18 15:49:08 +0300

    Adding boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherCard from './WeatherCard';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <WeatherCard />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class WeatherCard extends React.Component {
+  render() {
+    return(
+      <div>
+        WeatherCard
+      </div>
+    );
+  }
+}
+
+export default WeatherCard;
