b996855d1dc5e9530466d3f7cbfc77b10d2ab972
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2019-07-18 16:21:33 +0300

    Adding initial styling

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './WeatherCard.css';
 
 class WeatherCard extends React.Component {
   render() {
     return(
-      <div>
+      <div className="container">
         WeatherCard
       </div>
     );
