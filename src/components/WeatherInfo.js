03584a395b525ecc7d4a4f539a4d74d305ef735f
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2019-07-19 16:36:05 +0300

    Fetching data with axios

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 class WeatherInfo extends React.Component {
+  componentDidMount() {
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London&APPID=818b740e822277fb1688acee53285bde')
+    .then((response) => console.log(response))
+    .catch((error) => window.alert(error.message));
+  }
   render() {
     return(
       <div>
