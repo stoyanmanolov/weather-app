72e59d7c83b09aa78c3e16a97a73720fbb64ccb8
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2019-07-29 15:54:55 +0300

    Adding hooks for styling weather items onClick

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const WeatherList = ({ weather , onClick}) => {
+const WeatherList = ({ weather , onClick }) => {
+
+  const [ stateKey, setStateKey ] = useState();
 
   const listItems = (list) => {
     return Object.keys(list).map((key) =>
-    <li onClick={() => onClick(key)} key={key}>
+    <li style={ stateKey === key ? { backgroundColor: 'lightgrey' } : null } onClick={() => {
+      setStateKey(key);
+      onClick(key);
+    }} key={key}>
       <p>{list[key].dt.toLocaleDateString('default', { weekday: 'long' })}</p>
       <img src={`http://openweathermap.org/img/w/${list[key].weather[0].icon}.png`} alt="weatherIcon"></img>
       <p>{`${list[key].main.temp.toPrecision(2)}°`}</p>
